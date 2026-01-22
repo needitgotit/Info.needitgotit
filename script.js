8f5034ed5d298a2c01ab38421f2e7e83561d88eb
needitgotit <info.needitgotit@gmail.com>
2026-01-22 12:39:02 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", () => {
+ document.addEventListener("DOMContentLoaded", () => {
 
   const supabase = window.supabase.createClient(
     "https://kuabmauutjchvvrfycxk.supabase.co",
